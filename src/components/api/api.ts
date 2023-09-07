e4496698618faa22b87bab1f968aee665a9ae6b5
Your Name <you@example.com>
2023-09-07 16:31:29 +0800

    修改接口

--- src/components/api/api.ts
+++ src/components/api/api.ts
@@ -8,7 +8,7 @@ function getAPIUrl() {
     if (/^192\.168/.test(hostname) || /^10./.test(hostname)) {
         return "http://" + hostname + ":20000/v2/api"
     }
-    return "/v2/api"
+    return "http://www.mycube.club/v2/api"
 }
 
 export const API = new apiCore(getAPIUrl());
